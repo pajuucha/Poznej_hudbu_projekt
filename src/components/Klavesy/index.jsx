41c9989e873bc4323244df4ecd412d6a7abad989
Pavla Varechová <pavla.varechova@gmail.com>
2021-05-29 11:51:00 +0200

    klavesy_3

--- src/components/Klavesy/index.jsx
+++ src/components/Klavesy/index.jsx
@@ -7,11 +7,11 @@ export const Klavesy = (props) => {
 
   useEffect(() => {
     console.log('start');
-    window.addEventListener('keypress', keepPlay);
+    document.addEventListener('keypress', keepPlay);
 
     return () => {
       console.log('stop');
-      window.removeEventListener('keypress', keepPlay);
+      document.removeEventListener('keypress', keepPlay);
     };
   }, []);
 
@@ -111,21 +111,140 @@ export const Klavesy = (props) => {
     }
   };
 
+  const changeC1 = () => {
+    if (nota === 'playC1') {
+      return 'bila is-active';
+    }
+    return 'bila';
+  };
+  const changeD = () => {
+    if (nota === 'playD') {
+      return 'bila is-active';
+    }
+    return 'bila';
+  };
+  const changeE = () => {
+    if (nota === 'playE') {
+      return 'bila is-active';
+    }
+    return 'bila';
+  };
+  const changeF = () => {
+    if (nota === 'playF') {
+      return 'bila is-active';
+    }
+    return 'bila';
+  };
+  const changeG = () => {
+    if (nota === 'playG') {
+      return 'bila is-active';
+    }
+    return 'bila';
+  };
+  const changeA = () => {
+    if (nota === 'playA') {
+      return 'bila is-active';
+    }
+    return 'bila';
+  };
+  const changeH = () => {
+    if (nota === 'playH') {
+      return 'bila is-active';
+    }
+    return 'bila';
+  };
+  const changeC2 = () => {
+    if (nota === 'playC2') {
+      return 'bila is-active';
+    }
+    return 'bila';
+  };
+
+  const changeCis = () => {
+    if (nota === 'playCis') {
+      return 'cerna cerna--cis is-active';
+    }
+    return 'cerna cerna--cis';
+  };
+
+  const changeDis = () => {
+    if (nota === 'playDis') {
+      return 'cerna cerna--dis is-active';
+    }
+    return 'cerna cerna--dis';
+  };
+
+  const changeFis = () => {
+    if (nota === 'playFis') {
+      return 'cerna cerna--fis is-active';
+    }
+    return 'cerna cerna--fis';
+  };
+  const changeGis = () => {
+    if (nota === 'playGis') {
+      return 'cerna cerna--gis is-active';
+    }
+    return 'cerna cerna--gis';
+  };
+  const changeAis = () => {
+    if (nota === 'playAis') {
+      return 'cerna cerna--ais is-active';
+    }
+    return 'cerna cerna--ais';
+  };
+
   return (
     <div className="klavesy">
-      <button className="bila bila--c" onClick={playC1}></button>
-      <button className="cerna cerna--cis" onClick={playCis}></button>
-      <button className="bila bila--d" onClick={playD}></button>
-      <button className="cerna cerna--dis" onClick={playDis}></button>
-      <button className="bila bila--e" onClick={playE}></button>
-      <button className="bila bila--f" onClick={playF}></button>
-      <button className="cerna cerna--fis" onClick={playFis}></button>
-      <button className="bila bila--g" onClick={playG}></button>
-      <button className="cerna cerna--gis" onClick={playGis}></button>
-      <button className="bila bila--a" onClick={playA}></button>
-      <button className="cerna cerna--ais" onClick={playAis}></button>
-      <button className="bila bila--h" onClick={playH}></button>
-      <button className="bila bila--c2" onClick={playC2}></button>
+      <button className={changeC1()} onClick={playC1}>
+        <span class="bila__letter">S</span>
+      </button>
+
+      <button className={changeCis()} onClick={playCis}>
+        <span class="cerna__letter">D</span>
+      </button>
+
+      <button className={changeD()} onClick={playD}>
+        <span class="bila__letter">F</span>
+      </button>
+
+      <button className={changeDis()} onClick={playDis}>
+        <span class="cerna__letter">G</span>
+      </button>
+
+      <button className={changeE()} onClick={playE}>
+        <span class="bila__letter">H</span>
+      </button>
+
+      <button className={changeF()} onClick={playF}>
+        <span class="bila__letter">J</span>
+      </button>
+      <button className={changeFis()} onClick={playFis}>
+        <span class="cerna__letter">K</span>
+      </button>
+
+      <button className={changeG()} onClick={playG}>
+        <span class="bila__letter">L</span>
+      </button>
+
+      <button className={changeGis()} onClick={playGis}>
+        <span class="cerna__letter">E</span>
+      </button>
+
+      <button className={changeA()} onClick={playA}>
+        <span class="bila__letter">R</span>
+      </button>
+
+      <button className={changeAis()} onClick={playAis}>
+        <span class="cerna__letter">Z</span>
+      </button>
+
+      <button className={changeH()} onClick={playH}>
+        <span class="bila__letter">U</span>
+      </button>
+
+      <button className={changeC2()} onClick={playC2}>
+        <span class="bila__letter">I</span>
+      </button>
     </div>
   );
 };
